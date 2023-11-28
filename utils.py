7a4a2d846fcd0f259915d72f708d0acd73cc7fdb
GabrielCrl <108656807+GabrielCrl@users.noreply.github.com>
2023-11-28 12:50:53 +0100

    Update utils.py : added demoToGIF

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import re
 import cv2
+import imageio
 import vizdoom
 
 """
@@ -33,3 +34,26 @@ def demoToMP4(video_name, demo_file, config_file, resolution=vizdoom.ScreenResol
     game.close()
     video.release()
     cv2.destroyAllWindows()
+
+
+def demoToGIF(GIF_name, demo_file, config_file, resolution=vizdoom.ScreenResolution.RES_640X480):
+    r = re.findall(r'\d+', resolution.name)
+    vid_res = int(r[0]), int(r[1])
+
+    game = vizdoom.DoomGame()
+    game.load_config( config_file )
+    game.set_screen_format( vizdoom.ScreenFormat.CRCGCB )
+    game.set_screen_resolution(resolution)
+    game.set_window_visible(False)
+    game.set_render_hud(True)
+    game.init()
+    game.replay_episode(demo_file)
+    
+    images = []
+    while not game.is_episode_finished():
+        state = game.get_state()
+        img = np.moveaxis(state.screen_buffer,0, 2)
+        images.append(img)
+        game.advance_action(1)
+    game.close()
+    imageio.mimsave(GIF_name, images, duration = 1/35)
