205f1b3afc6b96f68a0a5cbc6c11e5503ae2f200
mathoufl <math.corteel@gmail.com>
2023-11-29 12:11:21 +0100

    commit fin CNN

--- model.py
+++ model.py
@@ -11,25 +11,35 @@ class Model(torch.nn.Module):
         super().__init__()
         self.conv1 = torch.nn.Sequential(
             torch.nn.Conv2d(4, 8, kernel_size=3, stride=2),
-            torch.nn.AvgPool2d(2,2),
+            torch.nn.AvgPool2d((2,2)),
             torch.nn.ReLU(),
         )
 
         self.conv2 = torch.nn.Sequential(
+            torch.nn.Conv2d(8, 8, kernel_size=3, stride=2),
+            torch.nn.AvgPool2d((2,2)),
+            torch.nn.ReLU(),
+        )
+
+        self.conv3 = torch.nn.Sequential(
             torch.nn.Conv2d(8, 16, kernel_size=3, stride=1),
-            torch.nn.AvgPool2d(2,2),
+            torch.nn.AvgPool2d((4,4)),
             torch.nn.ReLU(),
         )
 
         self.flatten = torch.nn.Flatten()
-        self.state = torch.nn.Linear(96, 1)
-        self.advantage = torch.nn.Linear(96, model.available_actions_count)
+        self.state_value = torch.nn.Linear(128, 1)
+        self.advantage = torch.nn.Linear(128, 108)
     
     def forward(self, input) :
         output = self.conv1(input)
         output = self.conv2(output)
+        output = self.conv3(output)
         output = self.flatten(output)
-        print(output.size())
+        state_value = self.state_value(output)
+        advantage = self.advantage(output)
+        return(state_value, advantage)
+        
 
 class Agent():
 
