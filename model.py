04c407292f042b8c0487ccffcc1e4a34566e484b
mathoufl <math.corteel@gmail.com>
2023-11-29 09:24:35 +0100

    rename classes.py del model.py

--- model.py
+++ model.py
@@ -1,21 +1,41 @@
-shoot = [1, 0, 0, 0, 0, 0, 0, 0]
-sprint = [0, 1, 0, 0, 0, 0, 0, 0]
-left = [0, 0, 0, 1, 0, 0, 0, 0]
-right = [0, 0, 1, 0, 0, 0, 0, 0]
-forward = [0, 0, 0, 0, 0, 1, 0, 0]
-backward = [0, 0, 0, 0, 1, 0, 0, 0]
-turn_left = [0, 0, 0, 0, 0, 0, 0, 1]
-turn_right = [0, 0, 0, 0, 0, 0, 1, 0]
+import torch
+import model
 
-actions = [
-    shoot,
-    sprint,
-    right,
-    left,
-    backward,
-    forward,
-    turn_right,
-    turn_left
-]
+class Model(torch.nn.Module):
+    """
+    class représentant notre model sur lequel nous feront de l'apprentissage profond
+        => permet d'inferer les action les plus efficaces à faire à partir d'un état donné
+    """
 
-available_actions_count = len(actions)
+    def __init__(self) :
+        super().__init__()
+        self.conv1 = torch.nn.Sequential(
+            torch.nn.Conv2d(4, 8, kernel_size=3, stride=2),
+            torch.nn.AvgPool2d(2,2),
+            torch.nn.ReLU(),
+        )
+
+        self.conv2 = torch.nn.Sequential(
+            torch.nn.Conv2d(8, 16, kernel_size=3, stride=1),
+            torch.nn.AvgPool2d(2,2),
+            torch.nn.ReLU(),
+        )
+
+        self.flatten = torch.nn.Flatten()
+        self.state = torch.nn.Linear(96, 1)
+        self.advantage = torch.nn.Linear(96, model.available_actions_count)
+    
+    def forward(self, input) :
+        output = self.conv1(input)
+        output = self.conv2(output)
+        output = self.flatten(output)
+        print(output.size())
+
+class Agent():
+
+    def __init__(self):
+        self.model = Model()
+
+
+
+        
